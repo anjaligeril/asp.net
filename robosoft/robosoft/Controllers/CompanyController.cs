73b49370bbc531e62e9d26caa88e3b0e4b14637f
anjaligeril <anjali99malu@gmail.com>
2018-11-26 18:16:42 -0500

    update nov26

---
+++ robosoft/robosoft/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace robosoft
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Leadership()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
